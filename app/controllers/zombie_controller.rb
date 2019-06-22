39bdce173a005fa3ea514738a2176a7cc54788a7
ameliaknipp <ameliaknipp@aol.com>
2019-06-22 20:08:29 +0000

    Index

---
+++ app/controllers/zombie_controller.rb
@@ -0,0 +1,5 @@
+class ZombieController < ApplicationController
+  def index
+  end
+  
+end
---
+++ test/controllers/zombie_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ZombieControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
